9fa38bcc6aaff3347818792ff05bc8e7f4308caf
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2025-10-22 11:27:04 +0800

    feat: pretty print example

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import mdast
+from pathlib import Path
+from pprint import pprint
+
+CURDIR = Path(__file__).parent
+
+EXAMPLE_FILE = CURDIR / "docs" / "example.md"
+
+
 def main():
-    print("Hello from mdast-python-examples!")
+    with open(EXAMPLE_FILE, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    parse_options = mdast.ParseOptions.gfm()
+    output = mdast.md_to_ast(text, parse_options)
+    pprint(output, sort_dicts=False)
 
 
 if __name__ == "__main__":
